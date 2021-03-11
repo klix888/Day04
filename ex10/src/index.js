2bb91ba3cf3e0573b6717ba1201870aebbdd4c92
Dzevad <ynwa886@gmail.com>
2021-03-11 17:21:25 +0100

    Declare String Variables

---
+++ ex10/src/index.js
@@ -0,0 +1,21 @@
+function main () {
+    //Only change code below this line 
+    var myFirstName = "Dzevad";
+    var myLastName = "Kliko";
+    var myHomeTown = "Jajce";
+
+    //Only change code above this line
+    return {
+    
+        myFirstName,
+        myLastName,
+        myHomeTown
+
+    } ;
+
+    
+   }
+
+
+console.log(main());  
+module.exports = main;
